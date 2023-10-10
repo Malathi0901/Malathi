07eb96d9ae633e14d622a2fe1487fa07e007f092
Malathi0901 <147466497+Malathi0901@users.noreply.github.com>
2023-10-10 09:21:57 +0530

    Add files via upload

---
+++ matplot.py
@@ -0,0 +1,49 @@
+# import matplotlib.pyplot as plt
+# # '''plt.plot([1, 2, 6, 4])
+# # plt.ylabel('guvi')
+# # plt.xlabel("something")
+# # plt.show()
+# # ax = plt.axes()
+# # ax.set_facecolor("#113111")
+# # plt.plot([1, 2, 3, 4,5,6,7,8,9,10], [1, 4, 9, 16,10,11,12,13,11,11], 'dr',markersize=5)
+
+
+# # plt.xlabel("guvi",fontsize=15,color="g",style="oblique",weight="bold")
+# # plt.axis([0, 20, 0, 20])#[x axis min,xaxis max,yaxis min,yaxis max]
+
+
+# # plt.show()
+
+# # plt.plot([1,2,3,4,], [1,4,9,16],'b',markersize=20)
+# # #plt.axis([0,10,0,30])
+# # plt.show() '''
+
+# # objects = ('Python', 'C++', 'Java', 'Perl', 'Scala')
+# # performance = [10,8,6,4,2]
+# # plt.figure(facecolor='yellow')
+
+
+# # neth=plt.axes()
+# # neth.set_facecolor("#66C328")
+
+
+# # plt.bar(objects, performance, align='center', alpha=0.5,color="#555",width=0.8,hatch="/")
+
+# # plt.show()
+
+
+# # #plt.barh(performance,width=1)
+# # #Matplotlib allows you to regulate the transparency of a graph plot using the alpha attribute. By default, alpha=1
+
+
+# import pandas as pd
+# df=pd.read_csv(r"https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/COMMONWEALTH%20GAMES%201930-2018%20%20-%20MEDAL%20TALLY.csv")
+# print(df.head(5))
+
+# matplot.py
+import streamlit as st
+
+# Streamlit code goes here
+st.title("Hello Streamlit")
+st.write("This is a simple Streamlit app.")
+
